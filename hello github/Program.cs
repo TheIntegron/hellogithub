138968fc4362352635494dc5c40a11cc407b071c
antonin.mudr <antonin.mudr@VT1-07>
2023-03-06 10:42:13 +0100

    Přidejte soubory projektu.

---
+++ hello github/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.WriteLine("Ahoj, jsem Antonín Mudr, chodím na na SŠ Educhem a mám rád/baví mě: ");
+string[] zajmy = { "cestování", "programování", "hraní her na pc", "dívání se na netflix", "volno", "spaní" };
+foreach(string predmet in zajmy)
+{
+    Console.WriteLine(predmet);
+}
+
+
+
+
+
